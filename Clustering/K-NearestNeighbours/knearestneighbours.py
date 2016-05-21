ff5391f62927b8b4a64ee413d9de383fb23e3454
DannyGotTea <danielgallagher500@gmail.com>
2016-05-21 13:01:27 +0100

    calc_distance updated & testing added

--- Clustering/K-NearestNeighbours/knearestneighbours.py
+++ Clustering/K-NearestNeighbours/knearestneighbours.py
@@ -4,26 +4,70 @@ import theano
 
 rand_gen = np.random
 
-def calc_distance(point1, point2):
-	if len(point1) != len(point2):
-		print ("Shapes not equal")
+def calc_distances(inputs, neighbours, k):
 
-	final_dist = 0
+	nb_feat = neighbours[:-1] #everything but last feature
+	target = neighbours[-1] #last feature
 
-	for feat1, feat2 in zip(point1, point2):
-		op = (feat1 - feat2) ** 2
-		final_dist += op
+	if inputs.shape != nb_feat.shape:
+		print("Shapes Not Equal: Inputs - {}, Neighbours - {}".format(inputs.shape, nb_feat.shape))
 
-	return np.sqrt(final_dist)
+	distances = T.sqrt((inputs - nb_feat) ** 2)
+
+	return distances[ : k-1]
 
 
-X = T.matrix('x')
-Y = T.vector('y')
 
-k = 3
+input_var = T.vector('input')
+nb = T.matrix('neighbours')
 
-seq = theano.tensor.arange(X)
+k = T.iscalar('k_value')
+seq_count = T.iscalar('sequnces_range')
+
+seq = T.arange(seq_count)
+
 output = T.as_tensor_variable(np.asarray(0, seq.dtype))
+
+scan_result, scan_updates = theano.scan(fn=calc_distances, outputs_info=None, sequences=nb, non_sequences=[input_var, k])
+
+knn = theano.function(inputs=[input_var, nb, k], outputs=scan_result)
+
+
+#Create samples for testing
+
+input_features = np.array([5.1, 3.8, 1.5, 0.3])
+
+# 0 = Iris-setosa, 1 = Iris-versicolor, 2 = Iris-virginica
+neighbours = np.array([
+[5.1,3.5,1.4,0.3,0],[5.7,3.8,1.7,0.3,0],[5.1,3.8,1.5,0.3,0],
+[5.4,3.4,1.7,0.2,0],[5.1,3.7,1.5,0.4,0],[4.6,3.6,1.0,0.2,0],
+[5.1,3.3,1.7,0.5,0],[4.8,3.4,1.9,0.2,0],[5.0,3.0,1.6,0.2,0],
+[5.0,3.4,1.6,0.4,0],[6.0,3.4,4.5,1.6,1],[6.7,3.1,4.7,1.5,1],
+[6.3,2.3,4.4,1.3,1],[5.6,3.0,4.1,1.3,1],[5.5,2.5,4.0,1.3,1],
+[5.5,2.6,4.4,1.2,1],[6.1,3.0,4.6,1.4,1],[5.8,2.6,4.0,1.2,1],
+[5.0,2.3,3.3,1.0,1],[5.6,2.7,4.2,1.3,1],[6.9,3.2,5.7,2.3,2],
+[5.6,2.8,4.9,2.0,2],[7.7,2.8,6.7,2.0,2],[6.3,2.7,4.9,1.8,2],
+[6.7,3.3,5.7,2.1,2],[7.2,3.2,6.0,1.8,2],[6.2,2.8,4.8,1.8,2],
+[6.1,3.0,4.9,1.8,2],[6.4,2.8,5.6,2.1,2],[7.2,3.0,5.8,1.6,2]])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
