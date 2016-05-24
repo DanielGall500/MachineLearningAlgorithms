8aaeef43a50dde7c2c137df288cfce67c0bb6c04
DannyGotTea <danielgallagher500@gmail.com>
2016-05-24 18:01:27 +0100

    K closest values to data sample found

--- Clustering/K-NearestNeighbours/knearestneighbours.py
+++ Clustering/K-NearestNeighbours/knearestneighbours.py
@@ -1,23 +1,20 @@
+from theano import function
 import numpy as np
 import theano.tensor as T
 import theano
+import operator
+
 
 rand_gen = np.random
 
 def calc_distances(step, inputs, neighbours, k):
 
-	nb_feat = neighbours[step] #everything but last feature
+	nb_feat = neighbours[step][:4] #everything but last feature
 	target = neighbours[-1] #last feature
 
-	print 'Input features: {}'.format(inputs)
-	print 'Neighbour features: {}'.format(nb_feat)
+	distance = T.sqrt(T.sum((inputs - nb_feat) ** 2)) #Euclidean distances
 
-	if inputs.shape != nb_feat.shape:
-		print("Shapes Not Equal: Inputs - {}, Neighbours - {}".format(inputs.shape, nb_feat.shape))
-
-	distances = T.sqrt((inputs - nb_feat) ** 2)
-
-	return step #distances #ERROR IS UP HERE
+	return distance
 
 input_var = T.matrix('input')
 nb = T.matrix('neighbours')
@@ -32,7 +29,18 @@ output_model = T.as_tensor_variable(np.asarray(0, step_count.dtype))
 
 scan_result, scan_updates = theano.scan(fn=calc_distances, outputs_info=None, sequences=seq, non_sequences=[input_var, nb, k])
 
-k_nearest_neighbours = theano.function(inputs=[input_var, nb, k, step_count], outputs=scan_result)
+distances = theano.function(inputs=[input_var, nb, k, step_count], outputs=scan_result)
+
+
+def k_nearest_neighbours(inputs, nb, k, steps): #TRYING TO FIGURE THIS OUT AND HOW TO DO IT (ONLY NEED TO GET K WORKING NOW)
+
+	distance_output = distances(input=inputs, neighbours=nb, k_value=k, steps=steps)
+
+	dict_out = dict(enumerate(distance_output))
+
+	k_nearest = (sorted(dict_out.items(), key=operator.itemgetter(1)))[:k+1] #Gets the k smallest (closest) values
+
+	print k_nearest
 
 
 #Create samples for testing
@@ -42,7 +50,7 @@ input_features = np.array([[5.1, 3.8, 1.5, 0.3]])
 k = 5
 
 neighbours = np.array([ # 0 = Iris-setosa, 1 = Iris-versicolor, 2 = Iris-virginica
-[5.1,3.5,1.4,0.3,0],[5.7,3.8,1.7,0.3,0],[5.1,3.8,1.5,0.3,0],
+[5.1,3.5,1.4,0.3,0],[5.7,3.8,1.7,0.3,0],[5.1,3.5,1.4,0.2,0],
 [5.4,3.4,1.7,0.2,0],[5.1,3.7,1.5,0.4,0],[4.6,3.6,1.0,0.2,0],
 [5.1,3.3,1.7,0.5,0],[4.8,3.4,1.9,0.2,0],[5.0,3.0,1.6,0.2,0],
 [5.0,3.4,1.6,0.4,0],[6.0,3.4,4.5,1.6,1],[6.7,3.1,4.7,1.5,1],
@@ -55,9 +63,7 @@ neighbours = np.array([ # 0 = Iris-setosa, 1 = Iris-versicolor, 2 = Iris-virgini
 
 steps = len(neighbours)
 
-knn = k_nearest_neighbours(input=input_features, neighbours=neighbours, k_value=k, steps=steps)
-print knn
-
+print k_nearest_neighbours(input_features, neighbours, k, steps)
 
 
 
